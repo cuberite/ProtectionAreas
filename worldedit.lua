407775c036bcbdd1ddc99b9cd63cbb52818b3594
William 'psyFi' Hatcher <william@hatcher.work>
2017-08-18 02:08:20 -0500

    Fixed styling

--- worldedit.lua
+++ worldedit.lua
@@ -12,18 +12,18 @@ end
 
 
 function PreventVandalism(a_Cuboid, a_Player)
-	Areas = g_PlayerAreas[a_Player:GetUniqueID()]
+	local Areas = g_PlayerAreas[a_Player:GetUniqueID()]
 	a_Cuboid:Sort()
-	local allow_vandalism = true
+	local allow_Vandalism = true
 
-	Areas:ForEachArea(function( area_Cuboid, is_allowed )
+	Areas:ForEachArea(function( area_Cuboid, is_Allowed )
 		if a_Cuboid:DoesIntersect(area_Cuboid) then -- The World Edit selection is in one of our areas!
-			allow_vandalism = is_allowed
+			allow_vandalism = is_Allowed
 		end
 	end)
 
-	if not(allow_vandalism) then
+	if not(allow_Vandalism) then
 		a_Player:SendMessageFailure("You cannot use WorldEdit in areas that you don't have access to!")
 	end
-	return not(allow_vandalism)
+	return not(allow_Vandalism)
 end
